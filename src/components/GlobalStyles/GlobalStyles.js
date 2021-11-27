c2c2ea7b02c578989b892c94ca2a4edfcf8d64ce
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-11-27 13:12:45 -0700

    added css variables to globalstyles

--- src/components/GlobalStyles/GlobalStyles.js
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components/macro';
+import { COLORS, WEIGHTS } from '../../constants';
 
 const GlobalStyles = createGlobalStyle`
 /* http://meyerweb.com/eric/tools/css/reset/
@@ -71,6 +72,21 @@ html {
     Silence the warning about missing Reach Dialog styles
   */
   --reach-dialog: 1;
+
+  /*
+    css variables for theming
+  */
+  --color-white: ${COLORS.white};
+  --color-gray-100: ${COLORS.gray[100]};
+  --color-gray-300: ${COLORS.gray[300]};
+  --color-gray-500: ${COLORS.gray[500]};
+  --color-gray-700: ${COLORS.gray[700]};
+  --color-gray-900: ${COLORS.gray[900]};
+  --color-primary: ${COLORS.primary};
+  --color-secondary: ${COLORS.secondary};
+  --weight-normal: ${WEIGHTS.normal};
+  --weight-medium: ${WEIGHTS.medium};
+  --weight-bold: ${WEIGHTS.bold};
 }
 
 html, body, #root {
